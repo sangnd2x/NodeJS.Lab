0d1163391f4a8459bc2403f63757a4ebd0d28ecc
sangnd2x <sanguyen128@gmail.com>
2022-09-16 09:57:01 +0700

    Lab4.1 Create middleware with Express

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000);
+// app.use((req, res, next) => {
+//     console.log('First middleware');
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log('Second middleware');
+//     res.send('<h1>Hello Mom!<h1>')
+// });
+
+app.use('/users', (req, res, next) => {
+    console.log('From "/user" middleware');
+    res.send('<p><ul><li>User 1</li><li>User 2</li><ul></p>')
+});
+
+app.use('/', (req, res, next) => {
+    console.log('From "/" middleware');
+    res.send('<h1>Hello there!</h1><h1>General Kenobi</h1><img src="https://i.pinimg.com/originals/40/74/60/407460925c9e419d82b93313f0b42f71.jpg"/>');
+});
+
+app.listen(3000);
