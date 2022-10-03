a17ebf4331a32787e68c1fcc62a9eeeaace92e64
sangnd2x <sanguyen128@gmail.com>
2022-10-03 10:56:43 +0700

    lab7.1 failed to fetch

--- server/server.js
+++ server/server.js
@@ -8,10 +8,7 @@ const server = express();
 const shopRoute = require('./routes/shop');
 const adminRoute = require('./routes/admin');
 
-server.use(cors({
-    origin: '*',
-    methods: 'POST, GET'
-}));
+server.use(cors());
 
 server.use(express.json({
     type: ['application/json']
@@ -20,7 +17,7 @@ server.use(express.json({
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(express.static(path.join(__dirname, 'public')));
 
+server.use(adminRoute);
 server.use(shopRoute);
-server.post(adminRoute);
 
 server.listen(5000);
