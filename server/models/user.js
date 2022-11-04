cd39cdcb17f94435334a09edeb9d4dab28bdca8e
sangnd2x <sanguyen128@gmail.com>
2022-11-04 14:56:13 +0700

    Lab14.2 commit

--- server/models/user.js
+++ server/models/user.js
@@ -21,29 +21,29 @@ const userSchema = new Schema({
     }
 });
 
-userSchema.methods.addToCart = function () {
-    const cartProductIndex = this.cart.items.findIndex(cp => {
-        return cp.productId.toString() === product._id.toString();
-    });
-    let newQuantity = 1;
-    const updatedCartItems = [...this.cart.items];
+// userSchema.methods.addToCart = function () {
+//     const cartProductIndex = this.cart.items.findIndex(cp => {
+//         return cp.productId.toString() === product._id.toString();
+//     });
+//     let newQuantity = 1;
+//     const updatedCartItems = [...this.cart.items];
 
-    if (cartProductIndex >= 0) {
-        newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-        updatedCartItems[cartProductIndex].quantity = newQuantity
-    } else {
-        updatedCartItems.push({
-            productId: new ObjectId(product._id),
-            quantity: newQuantity
-        })
-    }
-    const updatedCart = { items: updatedCartItems };
-    const db = getDb();
-    return db.collection('users').updateOne(
-        { _id: new ObjectId(this._id) },
-        { $set: { cart: updatedCart } }
-    );
-}
+//     if (cartProductIndex >= 0) {
+//         newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+//         updatedCartItems[cartProductIndex].quantity = newQuantity
+//     } else {
+//         updatedCartItems.push({
+//             productId: new ObjectId(product._id),
+//             quantity: newQuantity
+//         })
+//     }
+//     const updatedCart = { items: updatedCartItems };
+//     const db = getDb();
+//     return db.collection('users').updateOne(
+//         { _id: new ObjectId(this._id) },
+//         { $set: { cart: updatedCart } }
+//     );
+// }
 
 module.exports = mongoose.model('Users', userSchema);
 
