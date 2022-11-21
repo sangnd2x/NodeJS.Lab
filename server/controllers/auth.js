1c930277dc70f1f6273ec3ed3a69dbf5dfa077b7
sangnd2x <sanguyen128@gmail.com>
2022-11-21 10:41:41 +0700

    lab17.1 commit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
+const nodemailer = require('nodemailer');
 
 exports.postLogin = (req, res, next) => {
   const { email, password } = req.body;
@@ -21,7 +22,7 @@ exports.postLogin = (req, res, next) => {
               req.flash('loggedIn', 'Successfully logged in!');
               req.session.save((err) => {
                 if (err) console.log(err);
-                console.log('session created!', req.session.user);
+                console.log('session created!', req.session);
               })
               res.statusMessage = req.flash('loggedIn')
               res.status(200).end();
@@ -32,13 +33,31 @@ exports.postLogin = (req, res, next) => {
             }
           })
           .catch(err => console.log(err));
-    })
+      })
   }
 }
 
 exports.postSignup = (req, res, next) => {
   const { username, email, password, confirmPassword } = req.body;
   const errors = validationResult(req);
+
+  // Set up to send email
+  let transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+      user: 'sangtesting123@outlook.com',
+      pass: '123456789!@@!'
+    }
+  })
+
+  let mailDetails = {
+    from: 'sangtesting123@outlook.com',
+    to: 'derp12.08@gmail.com',
+    subject: 'Email from Node.js',
+    text: 'Success'
+  }
+
+  // Sign up
   if (!errors.isEmpty()) {
     console.log(errors.array())
     res.statusMessage = errors.array()[0].msg;
@@ -54,6 +73,13 @@ exports.postSignup = (req, res, next) => {
       });
       user.save()
         .then(result => {
+          transporter.sendMail(mailDetails, (err, info) => {
+            if (err) {
+              return console.log(err);
+            } else {
+              console.log('Email sent' + info.response);
+            }
+          })
           res.statusMessage = 'New user created!';
           return res.status(200).end();
       })
